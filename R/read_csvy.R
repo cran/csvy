4602562df58049941c0e9586f4e1cfb61f3dff6d
Thomas J. Leeper <thosjleeper@gmail.com>
2016-07-29 00:24:54 +0000

    version 0.1.3

---
+++ R/read_csvy.R
@@ -0,0 +1,122 @@
+#' @title Import CSVY data
+#' @description Import CSVY data as a data.frame
+#' @param file A character string or R connection specifying a file.
+#' @param sep A character string specifying a between-field separator. Passed to \code{\link[utils]{read.csv}} or \code{\link[data.table]{fread}} depending on the value of \code{method}. Ignored for \code{method = 'readr'}.
+#' @param dec A character string specifying a within-field separator. Passed to \code{\link[utils]{read.csv}} or \code{\link[data.table]{fread}} depending on the value of \code{method}. Ignored for \code{method = 'readr'}.
+#' @param header A character string or logical specifying whether the file contains a header row of column names (below the YAML frontmatter). Passed to \code{\link[utils]{read.csv}}, \code{\link[data.table]{fread}}, or \code{\link[readr]{read_csv}} depending on the value of \code{method}.
+#' @param stringsAsFactors A logical specifying whether to treat character columns as factors. Passed to \code{\link[utils]{read.csv}} or \code{\link[data.table]{fread}} depending on the value of \code{method}. Ignored for \code{method = 'readr'} which never returns factors.
+#' @param method A character string specifying which package to use to read in the CSV data. Must be on of \dQuote{utils} (for \code{\link[utils]{read.csv}}), \dQuote{data.table} (for \code{\link[data.table]{fread}}), or \dQuote{readr} (for \code{\link[readr]{read_csv}}).
+#' @param \dots Additional arguments passed to \code{\link[utils]{read.csv}}, \code{\link[data.table]{fread}}, or \code{\link[readr]{read_csv}} depending on the value of \code{method}.
+#' @examples
+#' read_csvy(system.file("examples", "example3.csvy", package = "csvy"))
+#' 
+#' @importFrom utils read.csv
+#' @importFrom yaml yaml.load
+#' @export
+#' @seealso \code{\link{write_csvy}}
+read_csvy <- function(file, sep = ",", dec = ".", header = "auto", stringsAsFactors = FALSE, 
+                      method = c("utils", "data.table", "readr"), ...) {
+    # read in whole file
+    f <- readLines(file)
+    if (!length(f)) {
+        stop("File does not exist or is empty")
+    }
+  
+    # identify yaml delimiters
+    g <- grep("^#?---", f)
+    if (length(g) > 2) {
+        stop("More than 2 yaml delimiters found in file")
+    } else if (length(g) == 1) {
+        stop("Only one yaml delimiter found")
+    } else if (length(g) == 0) {
+        stop("No yaml delimiters found")
+    }
+    
+    # extract yaml front matter and convert to R list
+    y <- f[(g[1]+1):(g[2]-1)]
+    if (all(grepl("^#", y))) {
+        y <- gsub("^#", "", y)
+    }
+    y <- yaml.load(paste(y, collapse = "\n"))
+    
+    # load the data
+    method <- match.arg(method)
+    dat <- paste0(f[(g[2]+1):length(f)], collapse = "\n")
+    if (method == "utils") {
+        out <- read.csv(text = dat, 
+                        sep = if (sep == "auto") "," else sep, 
+                        dec = if (dec == "auto") "." else dec, 
+                        stringsAsFactors = stringsAsFactors, ...)
+    } else if (method == "data.table") {
+        requireNamespace("data.table")
+        out <- data.table::fread(input = dat, 
+                                 sep = sep, sep2 = dec, header = header, 
+                                 stringsAsFactors = stringsAsFactors, ...)
+    } else if (method == "readr") {
+        requireNamespace("readr")
+        out <- readr::read_csv(file = dat, col_names = header, ...)
+    }
+  
+    # check metadata against header row
+    check_metadata(y, out)
+    
+    # add metadata to data
+    hnames <- lapply(y$fields, `[[`, "name")
+    for (i in seq_along(y$fields)) {
+        fields_this_col <- y[["fields"]][[match(names(out)[i], hnames)]]
+        if ("name" %in% names(fields_this_col)) {
+            fields_this_col[["name"]] <- NULL
+        }
+        if ("class" %in% names(fields_this_col)) {
+            if (fields_this_col[["class"]] == "factor") {
+                if (isTRUE(stringsAsFactors)) {
+                    try(out[,i] <- factor(out[,i], levels = fields_this_col[["levels"]]))
+                }
+            } else {
+                class(out[, i]) <- fields_this_col[["class"]]
+            }
+            fields_this_col[["class"]] <- NULL
+        }
+        attributes(out[, i]) <- fields_this_col
+        rm(fields_this_col)
+    }
+    y$fields <- NULL
+  
+    meta <- c(list(out), y)
+    out <- do.call("structure", meta)
+    out
+}
+
+check_metadata <- function(metadata, data) {
+    hnames <- lapply(metadata$fields, `[[`, "name")
+    
+    missing_from_metadata <- names(data)[!names(data) %in% hnames]
+    if (length(missing_from_metadata)) {
+        warning("Metadata is missing for ", 
+                ngettext(length(missing_from_metadata), "variable", "variables"), 
+                " listed in data: ", paste(missing_from_metadata, collapse = ", "))
+    }
+    
+    missing_from_data <- unlist(hnames)[!unlist(hnames) %in% names(data)]
+    if (length(missing_from_data)) {
+        warning("Data is missing for ", 
+                ngettext(length(missing_from_data), "variable", "variables"), 
+                " listed in frontmatter: ", paste(missing_from_metadata, collapse = ", "))
+    }
+    
+    duplicated_metadata <- unlist(hnames)[duplicated(unlist(hnames))]
+    if (length(duplicated_metadata)) {
+        warning("Duplicate metadata entries for ", 
+                ngettext(length(duplicated_metadata), "variable", "variables"), 
+                " listed in frontmatter: ", paste(duplicated_metadata, collapse = ", "))
+    }
+    
+    duplicated_columns <- unlist(hnames)[duplicated(unlist(hnames))]
+    if (length(duplicated_columns)) {
+        warning("Duplicate column names for ", 
+                ngettext(length(duplicated_columns), "variable", "variables"), 
+                ": ", paste(duplicated_metadata, collapse = ", "))
+    }
+    
+    NULL
+}
---
+++ README.md
@@ -0,0 +1,135 @@
+# Import and Export CSV Data With a YAML Metadata Header #
+
+CSVY is a file format that combines the simplicity of CSV (comma-separated values) with the metadata of other plain text and binary formats (JSON, XML, Stata, etc.). The [CSVY file specification](http://csvy.org/) is simple: place a YAML header on top of a regular CSV. 
+
+A CSVY file looks like this:
+
+```
+#---
+#name: my-dataset
+#fields:
+#  - name: var1
+#    title: variable 1
+#    type: string
+#    description: explaining var1
+#    constraints:
+#      - required: true
+#  - name: var2
+#    title: variable 2
+#    type: integer
+#  - name: var3
+#    title: variable 3
+#    type: number
+#---
+var1,var2,var3
+A,1,2.5
+B,3,4.3
+```
+
+Which we can read into R like this:
+
+
+
+```r
+library("csvy")
+str(read_csvy("inst/examples/readme.csvy"))
+```
+
+```
+## 'data.frame':	2 obs. of  3 variables:
+##  $ var1: atomic  A B
+##   ..- attr(*, "title")= chr "variable 1"
+##   ..- attr(*, "type")= chr "string"
+##   ..- attr(*, "description")= chr "explaining var1"
+##   ..- attr(*, "constraints")=List of 1
+##   .. ..$ :List of 1
+##   .. .. ..$ required: logi TRUE
+##  $ var2: atomic  1 3
+##   ..- attr(*, "title")= chr "variable 2"
+##   ..- attr(*, "type")= chr "integer"
+##  $ var3: atomic  2.5 4.3
+##   ..- attr(*, "title")= chr "variable 3"
+##   ..- attr(*, "type")= chr "number"
+##  - attr(*, "name")= chr "my-dataset"
+```
+
+Optional comment characters on the YAML lines make the data readable with any standard CSV parser while retaining the ability to import and export variable- and file-level metadata. The CSVY specification does not use these, but the csvy package for R does so that you (and other users) can continue to rely on `utils::read.csv()` or `readr::read_csv()` as usual. The `import()` in [rio](https://cran.r-project.org/package=rio) supports CSVY natively.
+
+To create a CSVY file from R, just do:
+
+
+```r
+library("csvy")
+library("datasets")
+write_csvy(iris, "iris.csvy")
+```
+
+```
+## Warning in write.table(file = file, x = x, append = TRUE, sep = sep, dec =
+## sep2, : appending column names to file
+```
+
+```
+## [1] "iris.csvy"
+```
+
+To read a CSVY into R, just do:
+
+
+```r
+d1 <- read_csvy("iris.csvy")
+str(d1)
+```
+
+```
+## 'data.frame':	150 obs. of  5 variables:
+##  $ Sepal.Length: num  5.1 4.9 4.7 4.6 5 5.4 4.6 5 4.4 4.9 ...
+##  $ Sepal.Width : num  3.5 3 3.2 3.1 3.6 3.9 3.4 3.4 2.9 3.1 ...
+##  $ Petal.Length: num  1.4 1.4 1.3 1.5 1.4 1.7 1.4 1.5 1.4 1.5 ...
+##  $ Petal.Width : num  0.2 0.2 0.2 0.2 0.2 0.4 0.3 0.2 0.2 0.1 ...
+##  $ Species     : atomic  setosa setosa setosa setosa ...
+##   ..- attr(*, "levels")= chr  "setosa" "versicolor" "virginica"
+```
+
+or use any other appropriate data import function to ignore the YAML metadata:
+
+
+```r
+d2 <- utils::read.table("iris.csvy", sep = ",")
+str(d2)
+```
+
+```
+## 'data.frame':	150 obs. of  5 variables:
+##  $ Sepal.Length: num  5.1 4.9 4.7 4.6 5 5.4 4.6 5 4.4 4.9 ...
+##  $ Sepal.Width : num  3.5 3 3.2 3.1 3.6 3.9 3.4 3.4 2.9 3.1 ...
+##  $ Petal.Length: num  1.4 1.4 1.3 1.5 1.4 1.7 1.4 1.5 1.4 1.5 ...
+##  $ Petal.Width : num  0.2 0.2 0.2 0.2 0.2 0.4 0.3 0.2 0.2 0.1 ...
+##  $ Species     : Factor w/ 3 levels "setosa","versicolor",..: 1 1 1 1 1 1 1 1 1 1 ...
+```
+
+
+
+## Package Installation ##
+
+The package is available on [CRAN](https://cran.r-project.org/package=csvy) and can be installed directly in R using:
+
+```R
+install.packages("csvy")
+```
+
+The latest development version on GitHub can be installed using **devtools**:
+
+```R
+if(!require("ghit")){
+    install.packages("ghit")
+}
+ghit::install_github("leeper/csvy")
+```
+
+[![CRAN Version](http://www.r-pkg.org/badges/version/csvy)](https://cran.r-project.org/package=csvy)
+![Downloads](http://cranlogs.r-pkg.org/badges/csvy)
+[![Travis-CI Build Status](https://travis-ci.org/leeper/csvy.png?branch=master)](https://travis-ci.org/leeper/csvy)
+[![Appveyor Build status](https://ci.appveyor.com/api/projects/status/40ua5l06jw0gjyjb?svg=true)](https://ci.appveyor.com/project/leeper/csvy)
+[![codecov.io](http://codecov.io/github/leeper/csvy/coverage.svg?branch=master)](http://codecov.io/github/leeper/csvy?branch=master)
+
